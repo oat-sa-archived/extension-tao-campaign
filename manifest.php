97b7323a7131537d5e624d5b976efe21f9fa4f3d
Ionut Padurariu <ionut@taotesting.com>
2017-09-15 14:50:18 +0200

    refactoring/TAO-5105/remove_globals_from_tao_core - use constants defined in tao-core

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'label' => 'Campaign management',
 	'description' => 'TAO Campaign extension',
     'license' => 'GPL-2.0',
-    'version' => '2.6',
+    'version' => '2.7.0',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
 		'taoDelivery' => '2.6'
